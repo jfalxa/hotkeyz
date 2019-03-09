980663a62f6aa7dd10f8a38167386156a4671fcc
Julien Falxa <jfalxa.sta@gmail.com>
2019-03-09 16:54:29 +0100

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+
+const pkg = require('./package.json')
+
+export default {
+  input: 'src/index.ts',
+
+  output: [
+    { file: pkg.main, name: 'keys', format: 'umd' },
+    { file: pkg.module, format: 'es' }
+  ],
+
+  external: ['tslib'],
+
+  plugins: [
+    resolve(),
+    commonjs({ include: 'node_modules/**' }),
+    typescript({ cacheRoot: '/tmp/.rpt2-cache' })
+  ]
+}
